4972f0a7d921542da148d1f579716b517868a2bd
Acacia Ackles <alackles@msu.edu>
2021-07-13 09:26:59 -0400

    Data preprocessing

--- analysis/01_branchial_analysis.R
+++ analysis/01_branchial_analysis.R
@@ -6,6 +6,11 @@
 #
 # @brief: Analyzes branchial arches within species
 
+
+###############################################
+# 0. DEFINITIONS
+###############################################
+
 # required packages
 library(ggplot2)
 library(dplyr)
@@ -17,9 +22,20 @@ proj.dir <- "~/Documents/research/paper-ebo/" #path to this repo
 data.dir <- paste(proj.dir, "data/", sep="")
 fig.dir <- paste (proj.dir, "figs/", sep="")
 
+###############################################
+# 1. DATA PRE-PROCESSING
+################################################
+
 # load branchial arch data
 branchial.data <- read.csv((paste(data.dir, "branchial_data.csv", sep="")))
 
 # Identify factors 
 facs <- c("Species", "Arch.ID", "Arch.Type")
 branchial.data[,facs] <- lapply(branchial.data[,facs], factor)
+
+# Grab the data we actually want: anchoa and brevoortia only
+ab.data <- branchial.data %>% 
+  filter(Species=="anchoa" | Species=="brevoortia") %>%
+  droplevels() %>%
+  {.}
+
