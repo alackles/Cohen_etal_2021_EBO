64eff8d45d407f85281b6be1ae58f1de660fcf30
Acacia Ackles <alackles@msu.edu>
2021-07-15 12:09:47 -0400

    Pull model slopes. Start model comp

--- analysis/02_branchial_model.R
+++ analysis/02_branchial_model.R
@@ -11,14 +11,15 @@
 # required packages
 library(lme4)
 library(lmerTest)
-library(ggeffects)
+library(dplyr)
+library(tidyr)
 
 # files
 proj.dir <- "~/Documents/research/paper-ebo/" #path to this repo
 data.dir <- paste(proj.dir, "data/", sep="")
 df.fname <- paste(data.dir, "branchial_df.Rdata", sep="")
 model.fname <- paste(data.dir, "model_df.Rdata", sep="")
-
+slopes.fname <- paste(data.dir, "model_slopes.csv")
 
 # load the required data
 ab.data <- readRDS(df.fname)
@@ -27,15 +28,20 @@ ab.data <- readRDS(df.fname)
 length.model <- lmer(data=ab.data, Arch.Length ~ SL*Species*Arch.Type + (0 + SL | Arch.Type:Arch.ID))
 ratio.model <- lmer(data=ab.data, Arch.Ratio ~ SL*Species*Arch.Type + (SL | Arch.Type:Arch.ID))
 
-# View the model data
-cb.effects.brev <- ggpredict(length.model, terms=c("Arch.ID", "Arch.Type [cerato]"), condition=c(Species="brevoortia"), type="random")
-cb.effects.anch <- ggpredict(length.model, terms=c("Arch.ID", "Arch.Type [cerato]"), condition=c(Species="anchoa"), type="random")
-eb.effects.brev <- ggpredict(length.model, terms=c("Arch.ID", "Arch.Type [epi]"), condition=c(Species="brevoortia"), type="random")
-eb.effects.anch <- ggpredict(length.model, terms=c("Arch.ID", "Arch.Type [epi]"), condition=c(Species="anchoa"), type="random")
-
 # add model fit to data
 ab.data$fitratio <- predict(ratio.model)
 ab.data$fitlength <- predict(length.model)
 
+# model slopes (i am so sorry)
+ab.slopes <- ab.data %>% 
+  group_by(Species, Arch.ID) %>%
+  arrange(SL) %>%
+  mutate(Slope = 
+           signif((last(Arch.Length) - first(Arch.Length))/(last(SL)-first(SL))), digits=3) %>%
+  dplyr::select(Species, Arch.ID, Slope) %>%
+  distinct() %>%
+  spread(Species, Slope)
+
 # Save and export dataframe with model fits attached for visualization 
 saveRDS(ab.data, file=model.fname)
+write.csv(ab.slopes, file=slopes.fname)
